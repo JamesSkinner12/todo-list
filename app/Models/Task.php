f64c214547a2db0a81db13fb3009317c3ebcfc85
James Skinner <james@Jamess-MacBook-Pro.local>
2021-11-30 15:24:48 -0500

    [ticket-number] Initial commit to set up repository with laravel install and migrations/models/controller stubs

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'tasks';
+    protected $guarded = [];
+
+    public function markCompleted($at = null)
+    {
+        $this->update([
+            'completed_at' => (!$at) ? now() : $at
+        ]);
+    }
+
+    public function getStatusAttribute()
+    {
+        return (!empty($this->completed_at)) ? "Complete" : "Incomplete";
+    }
+}
---
+++ database/migrations/2021_11_30_201317_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('order_number')->default(1);
+            $table->timestamp('completed_at')->default(null);
+            $table->timestamp('due_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
